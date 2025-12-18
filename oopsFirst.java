274b4214fda2379d6caf349fdd8a9fbd419f90ab
Akash Choudhary <akashkrchoudhary007@gmail.com>
2025-12-18 22:45:53 +0530

    g

--- oopsFirst.java
+++ oopsFirst.java
@@ -8,12 +8,19 @@ public class oopsFirst {
     }
     public static void main(String[] args) {
         // object 
-        Student akash = new Student();
+        Student akash = new Student();  //declearation
+        //initialization
         akash.name = "Akash";
         akash.rollNumber = 42;
         akash.percent = 99.99f;
 
         System.out.println(akash.name);
+
+        Student Rishu = new Student();
+        Rishu.name = "hui";
+        Rishu.rollNumber = 69;
+        Rishu.percent = 69.69f;
+        System.out.println(Rishu.percent);
         
     }
     
